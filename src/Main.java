50d83820e0cdfe75ea8a7ce711fba6a6b9dcfc0b
emirhankarakoc <emirhankarakoc@yahoo.com>
2023-01-06 23:20:58 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int sayi = 5000;
+        int test = 2;
+        while(test<=sayi){
+            System.out.println(test);
+            test *=2;
+        }
+    }
+}
